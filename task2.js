d055c868d4960f839e013afb81d5db0db0f401e5
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2021-10-11 16:35:07 +0300

    fixed issues

--- task2.js
+++ task2.js
@@ -1,15 +1,29 @@
 import csv from 'csvtojson'
 import * as fs from 'fs'
+import { pipeline } from 'stream'
 
 const csvFilePath = './csv/test.csv'
 
 const readStream = fs.createReadStream(csvFilePath);
-
 const writeStream = fs.createWriteStream('./output.txt');
+const csvToJsonStream = csv({
+    delimiter: ';',
+    colParser:{
+        "Amount":"number",
+        "Price":(item) => parseFloat(item.replace(',', '.')),
+    },
+    checkType:true
+})
 
-csv().fromStream(readStream).subscribe((json) => {
-    return new Promise((resolve) => {
-        writeStream.write(JSON.stringify(json) + '\n', (err) => err ? console.log(err) : null, )
-        resolve()
-    })
-}, (err) => console.log(err))
+pipeline(
+    readStream,
+    csvToJsonStream,
+    writeStream,
+    (err) => {
+        if (err) {
+            console.error('Pipeline failed.', err);
+        } else {
+            console.log('Pipeline succeeded.');
+        }
+    }
+);
